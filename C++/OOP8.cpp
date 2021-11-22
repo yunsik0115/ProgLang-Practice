5f5b9e7cbd04192553a19080cbc7aec0f36be10e
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-11-22 21:43:17 +0900

    Adding a Default Constructor to an existing class.

--- C++/OOP8.cpp
+++ C++/OOP8.cpp
@@ -13,6 +13,39 @@
 #include <string>
 #include <vector>
 
-// Creating Objects
-// Declaring a class
-//
+using namespace std;
+
+class Player
+{
+private:
+    std::string name;
+    int health;
+    int xp;
+
+public:
+    void set_name(std::string name_val){
+        name = name_val;
+    }
+    std::string get_name(){
+        return name;
+    }
+    /*Player() { // Default Initializer user-defined
+        name = "None";
+        health = 100;
+        xp = 3;
+    }*/
+    Player(std::string name_val, int health_val, int xp_val){
+        name = name_val;
+        health = health_val;
+        xp = xp_val;
+    }
+};
+
+int main(){
+    Player frank; // C++ Provides Default No args Constructor. If there is no user-defined constructor.
+    // If no args constructor not implemented but n args instructor implemented then object declaration without
+    // arguments will be prohibited and cause an error
+    frank.set_name("Frank");
+    cout << frank.get_name() << endl;
+    return 0;
+}
